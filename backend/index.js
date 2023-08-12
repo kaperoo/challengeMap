c1ff4a614f465564b90842459d05728fca1b3248
Wojtek <wojtek01259@gmail.com>
2023-08-12 16:50:55 +0200

    added nodemon configuration

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/users', (req, res)=>{
+    res.send('Hello users')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
