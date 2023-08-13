bdd381bf958e731e99039712c192def700ce2c32
Kacper Roemer <sc20kr@leeds.ac.uk>
2023-08-13 14:47:22 +0100

    map coordinates

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,10 +1,10 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import {continents} from "./continents";
 
 const Map = () => {
   return (
-    <div className="map">
     <MapContainer
       zoom={10}
       center={[51.505, -0.09]}
@@ -14,8 +14,38 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {
+        continents.features.map((continent) => {
+            const coordinates = continent.geometry.coordinates[0];
+            return (
+                <Polygon
+                    key={continent.properties.CONTINENT}
+                    pathOptions={{ 
+                        color: "white",
+                        fillColor: "purple",
+                        fillOpacity: 0.7,
+                        weight: 2,
+                        opacity: 1,
+                        dashArray: "3",
+
+                    }}
+                    positions={coordinates.map((coordinate) => [coordinate[1], coordinate[0]])}
+                    eventHandlers={{
+                        mouseover: (e) => {
+                            e.target.openPopup();
+                        },
+                        mouseout: (e) => {
+                            e.target.closePopup();
+                        },
+                        click: (e) => {
+                            console.log(e.target);
+                        }
+                    }}
+                    />
+            )
+        })
+      }
     </MapContainer>
-    </div>
   );
 };
 
